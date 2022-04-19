cd7607730da67b206d25fe0635c59c516be8864e
Hayden Faw <faw.hayden@gmail.com>
2022-04-18 22:17:38 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            return String.Equals(new string(word.Reverse().ToArray()), word, StringComparison.OrdinalIgnoreCase);
+
+            // ORIGINAL SOLUTION:
+            //string reverse = "";
+            //for (int i = word.Length - 1; i >= 0; i--)
+            //{
+            //    reverse += word[i];
+            //}
+            //return (word.ToLower() == reverse.ToLower()) ? true : false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,25 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("upgrade", false)]
+        [InlineData("level", true)]
+        public void Test1(string word, bool expected)
         {
+            //Arange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
